cbb2ab274f5ae4d9758cceca6e6f0bb12fed4e07
notbqx <sberrios543@gmail.com>
2026-02-27 02:22:23 -0400

    Added room code feature. UI not finished at all and need to add image/video support

--- src/main/java/com/sergio/chatgo/config/WebSocketEventListener.java
+++ src/main/java/com/sergio/chatgo/config/WebSocketEventListener.java
@@ -4,11 +4,17 @@ import com.sergio.chatgo.controller.ChatMessage;
 import com.sergio.chatgo.controller.MesssageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -21,11 +27,12 @@ public class WebSocketEventListener {
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
         String username = (String) headerAccessor.getSessionAttributes().get("username");
+        String roomId = (String) headerAccessor.getSessionAttributes().get("roomId");
 
-        if (username != null) {
-            log.info("User disconnected: {}", username);
+        if (username != null && roomId != null) {
+            log.info("User {} disconnected from room {}", username,  roomId);
             var chatMessage = ChatMessage.builder().type(MesssageType.LEAVE).sender(username).build();
-            messageTemplate.convertAndSend("/topic/public", chatMessage);
+            messageTemplate.convertAndSend("/topic/" + roomId, chatMessage);
         }
     }
 }
--- src/main/java/com/sergio/chatgo/controller/ChatController.java
+++ src/main/java/com/sergio/chatgo/controller/ChatController.java
@@ -4,21 +4,35 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public ChatController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
     @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
-    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-        return chatMessage;
+    public void sendMessage(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+
+        String roomId = (String) headerAccessor.getSessionAttributes().get("roomId");
+
+        if (roomId != null) {
+            messagingTemplate.convertAndSend("/topic/" + roomId, chatMessage);
+        }
     }
 
     @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+    public void addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        String roomId = chatMessage.getContent();
+
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        return chatMessage;
+        headerAccessor.getSessionAttributes().put("roomId", roomId);
+
+        messagingTemplate.convertAndSend("/topic/" + roomId, chatMessage);
     }
 }
